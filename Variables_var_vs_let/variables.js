cb6667b8840886993da26a4785b429b33b69889d
marincarlosandres42@gmail.com <marincarlosandres42@gmail.com>
2023-04-03 13:17:06 -0500

    Variables: var vs let

---
+++ Variables_var_vs_let/variables.js
@@ -0,0 +1,4 @@
+var a= 1 //esto es una variable con var
+let a = 1 //esto es una variable con let
+
+/*NOTA: es mas recomdable usar let, debido a probelmas que conlleva "var", por cuestoines de globalidad y scope*/ 
