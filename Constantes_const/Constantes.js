a4b0d74d527dc37d19dbe54cf9f795f7397b9d9f
marincarlosandres42@gmail.com <marincarlosandres42@gmail.com>
2023-04-17 12:35:18 -0500

    Constantes

---
+++ Constantes_const/Constantes.js
@@ -0,0 +1,5 @@
+/*Serefiere principalmente a una constante, a un valor no cambiante, es decir, tiene un valor fijo*/
+
+const PI= 3.1416 //Sabemos que Const es una valor fijo, no cambia
+                //NOTA: debemos decalrar "Const", siempre con una variable ya que no puede ir vacio
+                //Ademas,debemos usar Const, cuando sepamos que nuestro valor, de la variable no va a ser modificado
