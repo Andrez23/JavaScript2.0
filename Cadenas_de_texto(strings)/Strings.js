0de7e019117c5b792cfb01f00305407375abad62
marincarlosandres42@gmail.com <marincarlosandres42@gmail.com>
2023-04-18 11:02:01 -0500

    Cadenas de texto(Strings)

---
+++ Cadenas_de_texto(strings)/Strings.js
@@ -0,0 +1,26 @@
+//Cadenas de texto
+/*Es principalmente, texto, este va dentro de comillas 
+   Ej */
+
+let nombre1= "Andres"; //esto es una cadena de texto
+
+//Estas contienen 2 atributos muy importantes
+    //Propiedades: atributos que daran infromacion del objeto
+    //metodos: son las acciones que el objeto hace
+
+//Propiedades(solo algunas)
+    /* .lenght = nos permite ver cuantos caracteres tiene una cadena.Acordarse que los espacions tambien son considerados como caracteres
+    Ej  */
+
+let nombre2= "Andres";
+console.log(nombre.length);
+
+//Metodos(solo algunos)
+    /* .toUpperCase()= este obliga al usuario a escribir en letras MAYUSCULAS 
+       .toLowerCase()= este lo hace pero en minusculas
+    Ej */
+
+let nombre3= "Andres";
+
+console.log(nombre3.toUpperCase); 
+console.log(nombre3.toLowerCase);
