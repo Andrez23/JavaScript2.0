fcc75d697b89449a3625db3daaecf51d24797a00
marincarlosandres42@gmail.com <marincarlosandres42@gmail.com>
2023-04-18 12:37:05 -0500

    Numeros(Numbers)

---
+++ Numeros(Numbers)/Numeros.js
@@ -0,0 +1,12 @@
+//Numeros(Numbers)
+//Como su nomre lo indica, son numeros
+
+/*Metodos
+  Ej */
+
+let a= 2;
+let c= 7.19;
+
+console.log(c.toFixed(1)); /*Este nos permite dar cuantos valores decimales va a   tener la variable, sabemos que nos mostrata un solo numero, porque dimos un 1, en los parentesis */
+
+console.log(parseInt(c)); /*Este nos devuelve la parte entera de nuestro numero, en este caso, nos devilvera un 7, debido a que es la aprte entera*/ 
