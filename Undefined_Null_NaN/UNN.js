7569adc371b725ea4bef80e15eb98fcae134bca5
marincarlosandres42@gmail.com <marincarlosandres42@gmail.com>
2023-04-19 11:01:13 -0500

    Undefined, Null y NaN

---
+++ Undefined_Null_NaN/UNN.js
@@ -0,0 +1,21 @@
+//Undefined , Null y NaN
+
+/*Undefined: es prcinicpalmente una variable sin daos, sin valores, por ende JavaScript la detecta como indefinida
+   Ej */
+
+let indefinida;
+console.log(indefindida);
+
+/*Null: es parecia a "Undefined", pero esta se hace a proposito. Es decir, esta vacia, pero con ese mismo proposito
+    Ej */
+
+let nulo= null;
+console.log(null);
+
+/*NaN: signifa "Not a number". Y nos aparece cuando queremos hacer operaciones, pero deonde uno de los valores no es un numero
+    Ej */
+
+let noEsUNNumero= "hola"* 3.7;
+console.log(noEsUNNumero);
+
+
